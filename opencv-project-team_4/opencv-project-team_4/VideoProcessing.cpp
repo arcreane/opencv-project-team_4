9d2d4f04c645ca2fb8906feac8e58750b0d133d4
allan-benard <allandu97435@gmail.com>
2026-05-29 21:04:41 +0200

    Update VideoProcessing.cpp

--- opencv-project-team_4/opencv-project-team_4/VideoProcessing.cpp
+++ opencv-project-team_4/opencv-project-team_4/VideoProcessing.cpp
@@ -7,6 +7,7 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/video.hpp>
+#include <opencv2/calib3d.hpp>
 
 // ============================================================
 // Construction
